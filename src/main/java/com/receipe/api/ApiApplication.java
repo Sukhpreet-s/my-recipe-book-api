ea1d3a95c5bf1c27afbe254bf7d36f735499b9fa
unknown <sukhpreet-s>
2020-02-23 17:11:23 -0500

    added springbootservletinitializer in application file

--- src/main/java/com/receipe/api/ApiApplication.java
+++ src/main/java/com/receipe/api/ApiApplication.java
@@ -2,12 +2,27 @@ package com.receipe.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+//@SpringBootApplication
+//public class ApiApplication extends SpringBootServletInitializer {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(ApiApplication.class, args);
+//    }
+//
+//}
 
 @SpringBootApplication
-public class ApiApplication {
+public class ApiApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ApiApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
     }
-
 }
