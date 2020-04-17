adceb202a46b2bbda89fce844809758ce090137e
unknown <sukhpreet-s>
2020-04-16 23:09:27 -0400

    created index endpoint to test the application on cloud

--- src/main/java/com/receipe/api/ApiApplication.java
+++ src/main/java/com/receipe/api/ApiApplication.java
@@ -2,6 +2,8 @@ package com.receipe.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ApiApplication {
@@ -10,4 +12,13 @@ public class ApiApplication {
         SpringApplication.run(ApiApplication.class, args);
     }
 
+}
+
+@RestController
+class IndexController {
+
+    @GetMapping("/")
+    String hello(){
+        return "Application working!";
+    }
 }
