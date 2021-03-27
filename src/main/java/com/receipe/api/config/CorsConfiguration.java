67f535615bd42632f66bbd0068928dcde467f839
Sukhpreet Singh <sukhpreets6718@gmail.com>
2021-03-27 13:44:27 -0400

    add : added cors config to allow request from localhost temporarily

---
+++ src/main/java/com/receipe/api/config/CorsConfiguration.java
@@ -0,0 +1,29 @@
+package com.receipe.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Handles the cors configuration.
+ */
+@Configuration
+public class CorsConfiguration {
+    /**
+     * Cors configuration, currently allow localhost for development. Later change it to hosted frontend url.
+     * @return cors config. bean
+     */
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                    .addMapping("/**")
+                    .allowedMethods("POST")
+                    .allowedOrigins("http://localhost:4200");
+            }
+        };
+    }
+}
